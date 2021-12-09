9f11b9c9b6ece05f774c51bcd9ff3dd88529a875
makan dianka <makan.dianka@hotmail.com>
2021-12-09 02:17:59 +0000

    jeu-de-pair

--- style/js/main.js
+++ style/js/main.js
@@ -1,6 +1,17 @@
+/*
+    jeu de hazard, jeu de pair. jouer avec des carte, selectionner 2 cartes si les cartes séléctionné
+    sont identique alors vous avez gagné sinon vous avez perdu. dans tous les cas vous pouvez rejouer.
+    la position du cartes sont générer aléatoirement
+*/
+
+
+// Je recupere mon div dans la page html, avec container-card 
 const containerCard = document.getElementById("container-card")
+
+// les nom du fichier seront stocké ici
 let nameImg = []
 
+// la table card c'est le div principal et les 2 autres ce sont mes 2 lignes avec 4 cartes par lignes
 let card = [
     ['Dar1', 'Dar2', 'DrS1', 'DrS2'],
     ['Gho1', 'Gho2', 'Hul1', 'Hul2'],
@@ -8,6 +19,7 @@ let card = [
 
 jeuCard()
 
+// creation de mes cartes. cette fonction sert à créer 2 div et 4 button par ligne avec la face caché de la carte à defaut 
 function jeuCard(){
     var grid = ""
     for (let i = 0; i < card.length; i++){
@@ -23,6 +35,7 @@ function jeuCard(){
     containerCard.innerHTML = grid
 }
 
+// recuperation des noms des images dans le table card et ajouter l'extention jpg puis push dans nameImg
 function img(){   
     for (let i = 0; i < card.length; i++){
         for (let j = 0; j < card[i].length; j++){
@@ -30,9 +43,10 @@ function img(){
         }; 
     }
 } 
-  
 img()
 
+
+// cette fonction recupere le button et faire les tâches principal du programme
 function getBtn(){
     let btns = document.getElementsByClassName('btn')
     for (let i = 0; i < btns.length; i++){ 
@@ -72,7 +86,7 @@ function getBtn(){
 getBtn()
 
 
-
+// cette fonction sert à feliciter l'utilisateur qui gagne le jeu
 function felicitation(){
     let fel = document.getElementById('felicitation')
     fel.style.backgroundColor = 'green'
@@ -82,6 +96,7 @@ function felicitation(){
     fel.innerHTML = "Félicitation, vous avez gagné !"
 }
 
+// cette founction sert à afficher le desagrement si l'utilisateur échoue le jeu
 function navrer(){
     let navr = document.querySelector('.navrer')
     navr.style.backgroundColor = 'red'
@@ -91,6 +106,7 @@ function navrer(){
     navr.innerHTML = "Oups, vous avez perdu !"
 }
 
+// cette fonction, est un button de rejouer en cas de echoue ou reussite de l'utilisateur
 function retry(){
     let btn = document.getElementById('btn-retry')
     btn.innerHTML = '<button id="butt"><a href="/"><h2>Jouer encore</h2></a></button>'
