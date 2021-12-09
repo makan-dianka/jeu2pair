171e251235cd8ed85a48de06eb3bc27d3ebbfab6
makan dianka <makan.dianka@hotmail.com>
2021-12-09 01:41:42 +0000

    jeu-de-pair

--- style/js/main.js
+++ style/js/main.js
@@ -2,8 +2,8 @@ const containerCard = document.getElementById("container-card")
 let nameImg = []
 
 let card = [
-    ['Dardevil1', 'Dardevil2', 'DrStrange1', 'DrStrange2'],
-    ['Ghost-Rider1', 'Ghost-Rider2', 'Hulk1', 'Hulk2'],
+    ['Dar1', 'Dar2', 'DrS1', 'DrS2'],
+    ['Gho1', 'Gho2', 'Hul1', 'Hul2'],
 ]
 
 jeuCard()
@@ -36,10 +36,35 @@ img()
 function getBtn(){
     let btns = document.getElementsByClassName('btn')
     for (let i = 0; i < btns.length; i++){ 
-        let image = nameImg[i]
+        let randomImg = Math.floor(Math.random() * nameImg.length)
+        //let image = nameImg[i]
+        let image = nameImg[randomImg]
         let btn = btns[i]
         btn.addEventListener('click', () => {
-        btn.innerHTML = "<img src="+"style/img/"+image+" height=300 width=250>"
+        btn.innerHTML = "<img src="+"style/img/"+image+" class=img height=300 width=250>"
+        let div = document.getElementsByClassName("img")
+        if (div.length === 2){
+            let compare = []
+            for (element of div){
+                let test = element.getAttribute("src")
+                let spl = test.split('/')[2].slice(0, 3)
+                compare.push(spl)
+            }
+
+            if (compare[0] === compare[1]){
+                felicitation()
+                retry()
+                
+            }else{
+                navrer()
+                retry()
+                Location.reload()
+            }
+            
+
+        }else{
+            console.log('non')
+        }
         })
     } 
 }
@@ -47,3 +72,28 @@ function getBtn(){
 getBtn()
 
 
+
+function felicitation(){
+    let fel = document.getElementById('felicitation')
+    fel.style.backgroundColor = 'green'
+    fel.style.color = 'white'
+    fel.style.padding = '15px'
+    fel.style.textAlign = 'center'
+    fel.innerHTML = "Félicitation, vous avez gagné !"
+}
+
+function navrer(){
+    let navr = document.querySelector('.navrer')
+    navr.style.backgroundColor = 'red'
+    navr.style.color = 'white'
+    navr.style.padding = '15px'
+    navr.style.textAlign = 'center'
+    navr.innerHTML = "Oups, vous avez perdu !"
+}
+
+function retry(){
+    let btn = document.getElementById('btn-retry')
+    btn.innerHTML = '<button id="butt"><a href="/"><h2>Jouer encore</h2></a></button>'
+    btn.style.color = 'white'
+    btn.style.textAlign = 'center'
+}
